772fd8edd477e433b897dea9b32b991fa3692dc1
Brian <bmaitner@gmail.com>
2025-01-16 13:22:18 -0500

    trait citations

--- 4_BIEN_complete_citations.R
+++ 4_BIEN_complete_citations.R
@@ -1,6 +1,6 @@
 library(BIEN)
 
-# Get all of the unique datasource IDs needed for citing the occcurrence/plot records
+# Get all of the unique datasource IDs needed for citing the occurrence/plot records
 
   occ_ids <- BIEN:::.BIEN_sql("SELECT DISTINCT datasource_id
                     FROM view_full_occurrence_individual ;")
@@ -11,9 +11,34 @@ library(BIEN)
                     FROM agg_traits ;",
                                 fetch.query=FALSE)
 
-# Feed the traits into the citation function
+# Feed the IDs into the citation function
 
   full_citations <- BIEN_metadata_citation(dataframe = occ_ids,
                                            trait.dataframe = trait_ids,
                                            bibtex_file = "citations/BIEN_42_citations.bib",
                                            acknowledgement_file = "citations/BIEN_42_acknowledgements.txt")
+
+# Trait only data citations
+  
+  trait_citations <- BIEN_metadata_citation(trait.dataframe = trait_ids,
+                                           bibtex_file = "citations/BIEN_42_trait_citations.bib")
+  
+# Non-trait data citations
+  
+  
+
+# Total number of herbaria included? #804
+  
+  datasources <- occ_ids$datasource_id %>% na.omit()
+  
+  query<-paste("WITH a AS (SELECT * FROM datasource where datasource_id in (", paste(shQuote(datasources, type = "sh"),collapse = ', '),")) 
+                 SELECT * FROM datasource where datasource_id in (SELECT proximate_provider_datasource_id FROM a) OR datasource_id in (SELECT datasource_id FROM a) ;")
+  
+  sources <- BIEN:::.BIEN_sql(query,fetch.query = FALSE)
+
+  sources %>%
+    filter(sources$is_herbarium==1)%>%
+    select(source_name)%>%
+    unique()%>%nrow()
+  
+
